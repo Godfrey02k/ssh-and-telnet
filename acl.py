77406000c122a72ad922c04009572b63228956f2
Godfrey02k <149390540+Godfrey02k@users.noreply.github.com>
2023-12-08 00:45:11 +0000

    acl.py

--- acl.py
+++ acl.py
@@ -1,13 +1,20 @@
-session.sendline('access-list 1 permit any any')
-session.expect([r'R1\(config\)#', pexpect.TIMEOUT, pexpect.EOF])
+session.sendline('configure terminal')
+result = session.expect([r'#', pexpect.TIMEOUT])
+# Check for error, if exists then display error and exit
+if result != 0:
+    print('--- FAILURE! entering configuration mode')
+    exit()
+
+session.sendline('access-list 1 permit 192.168.1.0 0.0.0.255 any')
+session.expect([r'#', pexpect.TIMEOUT])
 
 session.sendline('interface GigabitEthernet0/0')
-session.expect([r'R1\(config-if\)#', pexpect.TIMEOUT, pexpect.EOF])
+session.expect([r'#', pexpect.TIMEOUT])
 session.sendline('ip access-group 1 in')
-session.expect([r'R1\(config-if\)#', pexpect.TIMEOUT, pexpect.EOF])
+session.expect([r'#', pexpect.TIMEOUT])
 
 
 session.sendline('end')
-session.expect([r'#', pexpect.TIMEOUT, pexpect.EOF])
+session.expect([r'#', pexpect.TIMEOUT])
 session.sendline('write memory')
-session.expect([r'#', pexpect.TIMEOUT, pexpect.EOF])
+session.expect([r'#', pexpect.TIMEOUT])
